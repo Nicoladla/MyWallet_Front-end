3e1fe0218e33ecff6923b61029ad35c7185955cf
Nicolas Araújo <nicolasaraujo@gmail.com>
2022-11-21 15:24:58 -0300

    Layout do componente de histórico de transação

--- src/Components/transactionHistory.js
+++ src/Components/transactionHistory.js
@@ -3,12 +3,19 @@ import styled from "styled-components";
 export default function TransactionHistory() {
   return (
     <BoxTransactionHistory>
-      <Transaction>
-        <p>
-          <span>24/11</span>Almoço com mãe
-        </p>
-        <p>39,90</p>
-      </Transaction>
+      <ul>
+        <Transaction>
+          <div>
+            <Date>24/11</Date>
+            <p>Almoço com mãe</p>
+          </div>
+          <Value>39,90</Value>
+        </Transaction>
+      </ul>
+      <footer>
+        <Saldo>SALDO</Saldo>
+        <Value>2000,90</Value>
+      </footer>
     </BoxTransactionHistory>
   );
 }
@@ -20,16 +27,41 @@ const BoxTransactionHistory = styled.main`
   margin-bottom: 13px;
   padding: 23px 12px 10px;
   border-radius: 5px;
-`;
-
-const Transaction = styled.section`
-  background-color: aquamarine;
-  font-size: 16px;
   display: flex;
+  flex-direction: column;
   justify-content: space-between;
 
-  span {
-    color: #C6C6C6;
-    margin-right: 10px;
+  ul {
+    width: 100%;
   }
+
+  footer {
+    display: flex;
+    justify-content: space-between;
+  }
+`;
+
+const Transaction = styled.li`
+  font-size: 16px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  div {
+    display: flex;
+    align-items: center;
+  }
+`;
+
+const Saldo = styled.p`
+  font-weight: bold;
+`;
+
+const Value = styled.p`
+  font-weight: 400;
+  color: red;
+`;
+const Date = styled.p`
+  color: #c6c6c6;
+  margin-right: 10px;
 `;
